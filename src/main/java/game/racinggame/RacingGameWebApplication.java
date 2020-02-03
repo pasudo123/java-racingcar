ded73acf2efb4b340b8392b973d8da05f29b2fbe
PARK SUNG DONG <oraedoa@gmail.com>
2020-02-03 09:03:42 +0900

    [feat] 스프링부트 전환 중.

---
+++ src/main/java/game/racinggame/RacingGameWebApplication.java
@@ -0,0 +1,12 @@
+package game.racinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingGameWebApplication {
+
+    public static void main(String[]args) {
+        SpringApplication.run(RacingGameWebApplication.class, args);
+    }
+}
